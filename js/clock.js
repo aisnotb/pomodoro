3ccf3512e22749c7e858d489dbbfccf50d48b085
huageng <stehliu25@gmail.com>
2017-05-09 22:53:17 +0800

    add or minus breaking time

---
+++ js/clock.js
@@ -0,0 +1,19 @@
+function Clock(){
+	var time = 0 ;
+
+	this.init = function(){
+
+	};
+
+	this.start =  function(){
+
+	};
+
+	this.update = function(){
+
+	};
+
+	this.stop =function(){
+
+	};
+}
--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-	
+	$('.minus').click(function(e){
+		console.log(e.target);
+		var break_time = parseInt($("#break").text());
+		console.log(break_time);
+		var new_time = (break_time - 1).toString();
+		if (new_time > 0) {
+			$("#break").text(new_time);
+		}else{
+			$("#break").text(0);	
+		}
+		
+	});
+
+	$('.add').click(function(){
+		alert('add clicked');
+	});
+
 });
