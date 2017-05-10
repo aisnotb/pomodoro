141f029979f12fdd66addfb6158d231a44aebc75
huageng <unstoppablediaosi@gmail.com>
2017-05-10 14:29:00 +0800

    test contribution

--- js/clock.js
+++ js/clock.js
@@ -1,6 +1,7 @@
 function Clock(){
 	this.time = 25 ;
 	var interval ;
+	var offset;
 
 	this.isOn = false;
 
@@ -15,17 +16,27 @@ function Clock(){
 	this.minus = function(){
 		if (this.time > 0) {
 			this.time--;
+		}else{
+			this.time = 0 ;
 		}
+	};
+
+	this.delta = function(){
+		var now = Date.now();
+		var timepassed = now - offset;
+		offset = now;
+		return timepassed;
 	};
 
 	this.start =  function(){
 		if (!this.isOn) {
 			this.isOn = false;
+			interval = setInterval(update, 1000);
 		}
 	};
 
 	this.update = function(){
-		
+
 	};
 
 	this.stop =function(){
